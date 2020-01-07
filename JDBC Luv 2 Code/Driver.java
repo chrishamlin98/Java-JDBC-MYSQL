7f92edf42731e88dad908a001fd6edd593ab797e
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2020-01-07 05:10:49 -0800

    Using JDBC connector from Luv 2 code

---
+++ JDBC Luv 2 Code/Driver.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		try {
+			//1. Get connection to database
+			
+			//2. Create a statement
+			
+			//3. Execute an SQL query
+			
+			//4. Process the result set
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
