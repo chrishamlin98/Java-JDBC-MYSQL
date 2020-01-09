23f2a278dbde45847d0a551741cd71650216f836
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2020-01-09 05:38:04 -0800

    Update Driver.java

--- JDBC Luv 2 Code/Driver.java
+++ JDBC Luv 2 Code/Driver.java
@@ -5,13 +5,18 @@ public class Driver {
 	public static void main(String[] args) {
 		try {
 			//1. Get connection to database
-			
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student" , "student");
 			//2. Create a statement
-			
+			Statement myStmt = myConn.createStatement();
 			//3. Execute an SQL query
-			
+			ResultSet myRs = myStmt.executeQuery("Select * from employees");
 			//4. Process the result set
-			
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
+
+			//  NEED TO INSERT DATA INTO THE MYSQL DATABASE TO SEE IF IT WORKS //
+
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
