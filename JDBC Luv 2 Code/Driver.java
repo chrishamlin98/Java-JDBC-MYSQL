4b59d0746672505f0419c92a2ba1f775bb4cc4fc
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2020-01-10 05:58:28 -0800

    Update Driver.java

--- JDBC Luv 2 Code/Driver.java
+++ JDBC Luv 2 Code/Driver.java
@@ -1,22 +1,38 @@
 import java.sql.*;
 
 public class Driver {
+	static boolean userRequestsData;
 
 	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/demo";
+		String user = "root";
+		String password = "root";
+
 		try {
 			//1. Get connection to database
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student" , "student");
+			Connection myConn = DriverManager.getConnection(url, user, password);
+
 			//2. Create a statement
 			Statement myStmt = myConn.createStatement();
-			//3. Execute an SQL query
+
+			//3. Execute a SQL update
+			String sql = "insert into employees "
+						+ " (last_name, first_name, email)"
+						+ " values ('Brown', 'David', 'david.brown@home.com')";
+
+			myStmt.executeUpdate(sql);
+
+			System.out.println("Insert complete.");
+
+/*			//3. Execute a SQL query
 			ResultSet myRs = myStmt.executeQuery("Select * from employees");
+
 			//4. Process the result set
 			while (myRs.next()) {
 				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
 			}
-
-			//  NEED TO INSERT DATA INTO THE MYSQL DATABASE TO SEE IF IT WORKS //
-
+*/
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
