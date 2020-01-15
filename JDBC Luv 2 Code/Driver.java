44e2c2629e3bea10a9f5bd1240ac7ebb7f7e87d7
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2020-01-15 05:14:24 -0800

    Update Driver.java
    Added helper method and changed statements to prepared statements

--- JDBC Luv 2 Code/Driver.java
+++ JDBC Luv 2 Code/Driver.java
@@ -3,28 +3,55 @@ import java.sql.*;
 public class Driver {
 	static boolean userRequest;
 
+	private static void display(ResultSet myRs) throws SQLException {
+		while (myRs.next()) {
+			String lastName = myRs.getString("last_name");
+			String firstName = myRs.getString("first_name");
+			double salary = myRs.getDouble("salary");
+			String department = myRs.getString("department");
+
+			System.out.printf("%s, %s, %.2f, %s\n", lastName, firstName, salary, department);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		String url = "jdbc:mysql://localhost:3306/demo?useSSL=false";
 		String user = "root";
 		String password = "root";
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
 
 		try {
 			//1. Get connection to database
-			Connection myConn = DriverManager.getConnection(url, user, password);
+			myConn = DriverManager.getConnection(url, user, password);
 
-			//2. Create a statement
-			Statement myStmt = myConn.createStatement();
+			//2. Prepare a statement
+			myStmt = myConn.prepareStatement("select * from employees where salary > ? and department=?");
 
-			//3. Execute SQL delete
-			String sql = "delete from employees where last_name='Brown'";
+			//3. Set the parameters
+			myStmt.setDouble(1, 80000);
+			myStmt.setString(2,  "Legal");
 
-			int rowsAffected = myStmt.executeUpdate(sql);
+			//4. Execute SQL query
+			myRs = myStmt.executeQuery();
 
-			System.out.println("Rows affected: " + rowsAffected);
-			System.out.println("Delete complete.");
+			//5. Display the result set
+			display(myRs);
 
-/*
+		}
+
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
+
+/* This is all of the code I experimented with that still works in different ways
+ *
+
  * import java.sql.*;
 
 /**
@@ -44,25 +71,31 @@ public class IncreaseSalariesForDepartment {
             // Get a connection to database
             myConn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/demo", "student", "student");
+
             String theDepartment = "Engineering";
             int theIncreaseAmount = 10000;
 
             // Show salaries BEFORE
             System.out.println("Salaries BEFORE\n");
             showSalaries(myConn, theDepartment);
+
             // Prepare the stored procedure call
             myStmt = myConn
                     .prepareCall("{call increase_salaries_for_department(?, ?)}");
+
             // Set the parameters
             myStmt.setString(1, theDepartment);
             myStmt.setDouble(2, theIncreaseAmount);
+
             // Call stored procedure
             System.out.println("\n\nCalling stored procedure.  increase_salaries_for_department('" + theDepartment + "', " + theIncreaseAmount + ")");
             myStmt.execute();
             System.out.println("Finished calling stored procedure");
+
             // Show salaries AFTER
             System.out.println("\n\nSalaries AFTER\n");
             showSalaries(myConn, theDepartment);
+
         } catch (Exception exc) {
             exc.printStackTrace();
         } finally {
@@ -124,6 +157,7 @@ public class IncreaseSalariesForDepartment {
 			String sql = "update employees "
 					+ " set email='demo@myhome.com'"
 					+ " where id=5";
+
 			myStmt.executeUpdate(sql);
 
 			System.out.println("Update complete.");
@@ -146,11 +180,3 @@ public class IncreaseSalariesForDepartment {
 				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
 			}
 */
-
-			}
-
-		catch (Exception exc) {
-			exc.printStackTrace();
-		}
-	}
-}
